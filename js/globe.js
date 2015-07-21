0a65af5ce0f988b6c356de10f88d727e11bcb5fa
esmyser <esmyser@gmail.com>
2015-07-20 21:37:19 -0400

    added delay on map show, spin, and cleaner code

--- js/globe.js
+++ js/globe.js
@@ -6,8 +6,10 @@ function initialize() {
     tilting: true,
     zooming: false,
     center: [0, 0],
-    zoom: 3
+    zoom: 3.5
   };
+
+  // overlay globe with earth images
   earth = new WE.map('earth_div', options);
   var natural = WE.tileLayer('http://data.webglearth.com/natural-earth-color/{z}/{x}/{y}.jpg', {
     tileSize: 256,
@@ -15,44 +17,42 @@ function initialize() {
   });
   natural.addTo(earth);
 
+
+  // overlay globe with place details
   var toner = WE.tileLayer('http://tile.stamen.com/toner/{z}/{x}/{y}.png', {
     attribution: 'Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap, under CC BY SA.',
     opacity: 0.3
   });
   toner.addTo(earth);
 
-//spinning
-  // var before = null;
-  //       requestAnimationFrame(function animate(now) {
-  //           var c = earth.getPosition();
-  //           var elapsed = before? now - before: 0;
-  //           before = now;
-  //           earth.setCenter([c[0], c[1] + 10*(elapsed/30)]);
-  //           requestAnimationFrame(animate);
-  //       });
-
+  // record latitude, longitude, and pass to googlemaps
   recordInfo = function(e) {
-    this.latitude = e.latitude;
-    this.longitude = e.longitude;
-    // trying to zoom into spot
-    // var bounds = [[this.latitude, this.longitude], [this.latitude + 1, this.longitude + 1]];
-    // earth.panInsideBounds(bounds)
-    initialize_map(Number(this.latitude), Number(this.longitude));
-    earth.hide()
-    }
+    var lt = Number(e.latitude),
+        lg = Number(e.longitude)
+
+    // zoom iify
+    zoom_in(lt, lg);
+
+    // show map
+    setTimeout(function(){initialize_map(lt, lg)}, 3000);
+  }
 
   earth.on('click', recordInfo)
 
+  zoom_in = function(lt, lg) {
+    var bounds = [[lt, lg], [(lt + 5), (lg + 5)]];
+    earth.panInsideBounds(bounds)
+    
+  }
 
-  function initialize_map(lt, lg) {
-    debugger
-    var lt = lt
-    var lg = lg
-    var mapOptions = {
-      center: { lat: lt, lng: lg},
-      zoom: 9,
-      mapTypeId: google.maps.MapTypeId.SATELLITE
-    };
+  initialize_map = function(lt, lg) {
+    var lt = lt,
+        lg = lg
+        mapOptions = {
+          center: { lat: lt, lng: lg},
+          zoom: 9,
+          mapTypeId: google.maps.MapTypeId.SATELLITE
+        };
 
     var map = new google.maps.Map(document.getElementById('map-canvas'),
         mapOptions);
