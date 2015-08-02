bc8b5f56bf3b772c83da9abecfbd2000235ff16a
esmyser <esmyser@gmail.com>
2015-08-02 14:54:58 -0400

    cleaned up streetview code so I could understand it better - just stylistic changes

--- js/genesis.js
+++ js/genesis.js
@@ -8,14 +8,6 @@
 function initialize(){
   earth = new World;
 
-  // $("#reset").on('click', function(){
-  //     alert("CLICKED");
-  //     earth = new World;
-  //     $(function(){earth.theBall.on('dblclick', earth.spinStop);})
-      
-      // toggleStreetView(); 
-  // });
-
   $(function(){
     $("#explore").click(earth.explore);
     $("#lucky").click(earth.lucky);
@@ -123,95 +115,94 @@ World.prototype.spinStop = function(event){
 World.prototype.googleMe = function(lt, lg) {
   // https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=cruise&key=API_KEY
   // var url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lt + "," + lg + "&radius=500&types=airport&key=AIzaSyDEFlHNtb2j5Jlixac6I3_vpX_SRAdYqxw"
-  
   $("#map-canvas").toggle();
+
   var currentPlace = { lat: lt, lng: lg};
   var mapOptions = {
-    center: currentPlace,
-    zoom: 10,
-    panControl: false,
-    zoomControl: true,
-    scaleControl: true,
-    zoomControlOptions: {
-      style: google.maps.ZoomControlStyle.SMALL
-    }
+      center: currentPlace,
+      zoom: 10,
+      panControl: false,
+      zoomControl: true,
+      scaleControl: true,
+      zoomControlOptions: { style: google.maps.ZoomControlStyle.SMALL }
   };
+  var mapCanvas = document.getElementById('map-canvas');
+
   //set up the map
-  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+      map.controls[google.maps.ControlPosition.TOP_RIGHT].push(centerControlDiv);
+  //  map.controls[google.maps.ZoomControlStyle.LARGE].push(centerControlDiv);
+
   //get streetview
   var sv = new google.maps.StreetViewService();
-  
-    var centerControlDiv = document.createElement('div');
-    centerControlDiv.index = 1;
-    var centerControl = new CenterControl(centerControlDiv, map);
-   
-    map.controls[google.maps.ControlPosition.TOP_RIGHT].push(centerControlDiv);
-    // map.controls[google.maps.ZoomControlStyle.LARGE].push(centerControlDiv);
-
-    function CenterControl(controlDiv, map) {
-      // Set CSS for the control border
-      var controlUI = document.createElement('div');
-      controlUI.style.backgroundColor = '#fff';
-      controlUI.style.border = '2px solid #fff';
-      controlUI.style.borderRadius = '3px';
-      controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
-      controlUI.style.cursor = 'pointer';
-      controlUI.style.marginBottom = '22px';
-      controlUI.style.textAlign = 'center';
-      
-      controlUI.title = 'reset button';
-      controlDiv.appendChild(controlUI);
-      // Set CSS for the control interior
-      var controlText = document.createElement('div');
-      controlText.style.color = 'rgb(25,25,25)';
-      controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
-      controlText.style.fontSize = '16px';
-      controlText.style.lineHeight = '38px';
-      controlText.style.paddingLeft = '5px';
-      controlText.style.paddingRight = '5px';
-      
-      controlText.innerHTML = 'RESET';
-      controlUI.appendChild(controlText);
-
-      // Setup the click event listeners: simply set the map to our currentlocation.
-      google.maps.event.addDomListener(controlUI, 'click', function() {
-        $('#map-canvas').toggle();
-        // $('#earth_div').toggle();
-          initialize();
-      });
-    }; //end function ResetControl
-
-
-
-    
-
-      // Set the initial Street View camera to the center of the map
+      // Set the initial Street View camera to the center of the maps
       sv.getPanorama({location: currentPlace, radius: 5000000}, processSVData);
-
       // Look for a nearby Street View panorama when the map is clicked.
       // getPanoramaByLocation will return the nearest pano when the
       // given radius is 500000 meters or less.
-      //leaving this commented gives you the ability to interact with the google map.
+
+      // leaving this commented gives you the ability to interact with the google map.
       // google.maps.event.addListener(map, 'click', function(event) {
       //   sv.getPanorama({location: currentPlace, radius: 500000}, processSVData);
       // });
+ 
+  var centerControlDiv = document.createElement('div');
+      centerControlDiv.index = 1;
+      centerControl = new CenterControl(centerControlDiv, map);
 
-    panorama = map.getStreetView();
-    panorama.setPosition(currentPlace);
-    panorama.controls[google.maps.ControlPosition.TOP_RIGHT].push(centerControlDiv);
-    
-    panorama.setPov(/** @type {google.maps.StreetViewPov} */({
-      heading: 265,
-      pitch: 0
-    }));
+  var panorama = map.getStreetView();
+      panorama.setPosition(currentPlace);
+      panorama.controls[google.maps.ControlPosition.TOP_RIGHT].push(centerControlDiv);
+      
+      panorama.setPov(/** @type {google.maps.StreetViewPov} */({
+        heading: 265,
+        pitch: 0
+      }));
+
+
+    function CenterControl(centerControlDiv, map) {
+      // Set CSS for the control border
+      var controlUI = document.createElement('div');
+          controlUI.style.backgroundColor = '#fff';
+          controlUI.style.border = '2px solid #fff';
+          controlUI.style.borderRadius = '3px';
+          controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+          controlUI.style.cursor = 'pointer';
+          controlUI.style.marginBottom = '22px';
+          controlUI.style.textAlign = 'center';
+          controlUI.title = 'reset button';
+  
+      // Set CSS for the control interior
+      var controlText = document.createElement('div');
+          controlText.style.color = 'rgb(25,25,25)';
+          controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
+          controlText.style.fontSize = '16px';
+          controlText.style.lineHeight = '38px';
+          controlText.style.paddingLeft = '5px';
+          controlText.style.paddingRight = '5px';
+          controlText.innerHTML = 'RESET';
+
+      // Combine Text and UI, add UI to DOM
+      controlUI.appendChild(controlText);
+      centerControlDiv.appendChild(controlUI);
+
+      // Listen for Reset click
+      google.maps.event.addDomListener(controlUI, 'click', function() {
+        $('#map-canvas').toggle();
+        initialize();
+        $('#earth_div').children().last().remove();
+        $('#earth_div').children().last().remove();
+      });
+    };
+
 
     function processSVData(data, status) {
       if (status == google.maps.StreetViewStatus.OK) {
-        var marker = new google.maps.Marker({
-          position: data.location.latLng,
-          map: map,
-          title: data.location.description
-        });
+          var marker = new google.maps.Marker({
+              position: data.location.latLng,
+              map: map,
+              title: data.location.description
+            });
 
         panorama.setPano(data.location.pano);
         panorama.setPov({
@@ -230,24 +221,9 @@ World.prototype.googleMe = function(lt, lg) {
           });
           panorama.setVisible(true);
         });
-      } else {
+      } 
+      else {
         console.error('Street View data not found for this location.');
       }
     };   
-}
-
-
-
-//  Ezra's old map code
-//   var lt = lt,
-//       lg = lg,
-//       mapOptions = {
-//         center: { lat: lt, lng: lg},
-//         zoom: 8,
-//         mapTypeId: google.maps.MapTypeId.SATELLITE
-//       };
-
-//   var map = new google.maps.Map(document.getElementById('map-canvas'),
-//       mapOptions);
-// }
-
+};
