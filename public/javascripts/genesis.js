afb60be90f982c5daaef73f1d804806e74c05c8f
esmyser <esmyser@gmail.com>
2016-04-09 23:11:20 -0400

    more refactoring:

--- public/javascripts/genesis.js
+++ public/javascripts/genesis.js
@@ -63,25 +63,155 @@ addRules = function(earth){
 mapIt = function(e) {
   var gMap = new GoogleMap();
 
-  var map          = gMap.map;
-  var controlDiv   = gMap.controlDiv;
-  var currentPlace = getPlace(e);
+  gMap.map.setCenter(currentPlace);
 
-  map.setCenter(currentPlace);
   $("#map-canvas").fadeToggle(900);
+
+  var currentPlace = getPlace(e);
   getStreetview(gMap, currentPlace);
-  addResetButton(controlDiv);
+ 
+  addResetButton(gMap.controlDiv);
 };
 
 getPlace = function(e){
-  var lat = Number(e.latitude);
-  var lng = Number(e.longitude);
   var currentPlace = {
-    lat : lat, 
-    lng : lng
+    lat : Number(e.latitude), 
+    lng : Number(e.longitude)
   };
 
   return currentPlace;
+};
+
+getStreetview = function(gMap, currentPlace){
+  var controlDiv = gMap.controlDiv;
+  var streetview = gMap.streetview;
+  var map        = gMap.map;
+
+  streetview.getPanorama({ 
+    location : currentPlace, 
+    radius   : 5000000 
+  }, processSVData);
+
+  function processSVData(data, status) {
+    if (status == google.maps.StreetViewStatus.OK) {
+      setupAndRenderStreetView(data, status, gMap, currentPlace);
+    } else {
+      console.error('Street View data not found for earth location.');
+    }
+  }
+};
+
+setupAndRenderStreetView = function(data, status, gMap, currentPlace){
+  var map        = gMap.map;
+  var controlDiv = gMap.controlDiv;
+  var position   = data.location.latLng;
+  var marker     = new google.maps.Marker({
+      position : position,
+      map      : map,
+      title    : data.location.description,
+      visible  : false
+  });
+
+  var panorama = map.getStreetView();
+      panorama.setPosition(currentPlace);
+      panorama.setOptions({ enableCloseButton : false });
+      panorama.controls[google.maps.ControlPosition.TOP_RIGHT].push(controlDiv);
+      panorama.setPano(data.location.pano);
+      panorama.setPov({heading: 270, pitch: 0});
+
+  // getting the location data from the marker
+  var geocoder = new google.maps.Geocoder();
+  
+  geocoder.geocode({ 'location': position }, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK && results[0]) {
+      var location = getLocation(results);
+      console.log(location);
+      appendLocationToDOM(map, location);
+    } else { 
+      console.log("Geocoder failed due to: " + status); 
+    }
+  });
+
+  setTimeout(function(){ marker.setVisible(true);         }, 1000);
+  setTimeout(function(){ map.panTo(marker.getPosition()); }, 1000);
+  setTimeout(function(){ panorama.setVisible(true);       }, 2500);
+};
+
+getLocation = function(results){
+  var result;
+  // check if we can get city level info
+  for (i=0; i<results.length; i++) {
+      if (results[i].types[0]=='locality') { 
+        result = results[i];
+        break; 
+    }
+  }
+
+  // if we don't have a city, we look for state
+  if (!result) {
+    for (i=0; i<results.length; i++) {
+        if (results[i].types[0]=='administrative_area_level_1') { 
+          result = results[i];
+          break; 
+      }
+    }
+  }
+
+  // if we don't have a state, we look for country
+  if (!result) {
+    for (i=0; i<results.length; i++) {
+        if (results[i].types[0]=='country') { 
+          result = results[i];
+          break; 
+      }
+    }
+  }
+
+  // making some easier variables, result.city, result.region, result.country
+  for (i=0; i<result.address_components.length; i++) {
+    if (result.address_components[i].types[0] == "locality") {
+            result.city = result.address_components[i].long_name;
+        }
+    if (result.address_components[i].types[0] == "administrative_area_level_1") {
+            result.region = result.address_components[i].long_name;
+        }
+    if (result.address_components[i].types[0] == "country") {
+            result.country = result.address_components[i].long_name;
+        }
+  }
+
+  var city     = result.city;
+  var region   = result.region;
+  var country  = result.country;
+  var location = "";
+
+  if (city) 
+    { location += city; }
+
+  if (city && region) 
+    { location += ", " + region; }
+  else if (region)    
+    { location += region; }
+
+  if ((city || region) && country) 
+    { location += ", " + country; }
+  else if (country)                
+    { location += country; }
+
+  return location;
+};
+
+appendLocationToDOM = function(map, location){
+  var myTitle = document.createElement('h4');
+      myTitle.style.color = 'white';
+      myTitle.innerHTML = "<p id='big_font'>" + location + "</p>";
+
+  var myTextDiv = document.createElement('div');
+      myTextDiv.appendChild(myTitle);
+
+  setTimeout(function(){ 
+    map.controls[google.maps.ControlPosition.TOP_CENTER].push(myTextDiv);
+  }, 1000);
 };
 
 addResetButton = function(controlDiv) {
@@ -119,124 +249,4 @@ styleText = function(textDiv){
   textDiv.style.paddingRight  = '5px';
   textDiv.innerHTML           = 'RESET';
   return textDiv;
-};
-
-getStreetview = function(gMap, currentPlace){
-  var controlDiv = gMap.controlDiv;
-  var streetview = gMap.streetview;
-  var map        = gMap.map;
-
-  streetview.getPanorama({ 
-    location : currentPlace, 
-    radius   : 5000000 
-  }, processSVData);
-
-  function processSVData(data, status) {
-    if (status == google.maps.StreetViewStatus.OK) {
-      something(data, status, gMap, currentPlace);
-    } else {
-      console.error('Street View data not found for earth location.');
-    }
-  }
-};
-
-something = function(data, status, gMap, currentPlace){
-  var map = gMap.map;
-  var controlDiv = gMap.controlDiv;
-  var marker = new google.maps.Marker({
-      position : data.location.latLng,
-      map      : map,
-      title    : data.location.description,
-      visible  : false
-  });
-
-  var panorama = map.getStreetView();
-      panorama.setPosition(currentPlace);
-      panorama.setOptions({ enableCloseButton : false });
-      panorama.controls[google.maps.ControlPosition.TOP_RIGHT].push(controlDiv);
-      panorama.setPano(data.location.pano);
-      panorama.setPov({heading: 270, pitch: 0});
-
-  // getting the location data from the marker
-  var geocoder = new google.maps.Geocoder();
-  geocoder.geocode({'location': marker.getPosition()}, function(results, status) {
-    if (status == google.maps.GeocoderStatus.OK && results[0]) {
-      var result;
-
-      // check if we can get city info
-      for (var i = 0; i < results.length; i++) {
-          if (results[i].types[0]=='locality') { 
-            result = results[i];
-            break; 
-        }
-      }
-
-      // if we don't have a city, we look for state
-      if (!result) {
-        for (var i = 0; i < results.length; i++) {
-            if (results[i].types[0]=='administrative_area_level_1') { 
-              result = results[i];
-              break; 
-          }
-        }
-      }
-
-      // if we don't have a state, we look for country
-      if (!result) {
-        for (var i = 0; i < results.length; i++) {
-            if (results[i].types[0]=='country') { 
-              result = results[i];
-              break; 
-          }
-        }
-      }
-
-      // making some easier variables, result.city, result.region, result.country
-      for (i=0; i<result.address_components.length; i++) {
-        if (result.address_components[i].types[0] == "locality") {
-                result.city = result.address_components[i].long_name;
-            }
-        if (result.address_components[i].types[0] == "administrative_area_level_1") {
-                result.region = result.address_components[i].long_name;
-            }
-        if (result.address_components[i].types[0] == "country") {
-                result.country = result.address_components[i].long_name;
-            }
-      }
-
-      var city     = result.city;
-      var region   = result.region;
-      var country  = result.country;
-      var location = "";
-
-      if (city) 
-        { location += city; }
-
-      if (city && region) 
-        { location += ", " + region; }
-      else if (region)    
-        { location += region; }
-
-      if ((city || region) && country) 
-        { location += ", " + country; }
-      else if (country)                
-        { location += country; }
-
-      console.log(location);
-
-      var myTitle = document.createElement('h4');
-      myTitle.style.color = 'white';
-      myTitle.innerHTML = "<p id='big_font'>" + location + "</p>";
-      var myTextDiv = document.createElement('div');
-      myTextDiv.appendChild(myTitle);
-      setTimeout(function(){ map.controls[google.maps.ControlPosition.TOP_CENTER].push(myTextDiv);}, 1000);
-
-    } else { 
-      console.log("Geocoder failed due to: " + status); 
-    }
-  });
-
-  setTimeout(function(){ marker.setVisible(true); }, 1000);
-  setTimeout(function(){ map.panTo(marker.getPosition()); }, 1000);
-  setTimeout(function(){ panorama.setVisible(true); }, 2500);
 };
